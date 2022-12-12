e97351a9626b605908b2a01115def95917d2a4b5
Anil <Anil@DESKTOP-FJTUG50>
2022-12-12 17:34:38 +0530

    Methods

---
+++ method/src/com/project/method/demo.java
@@ -0,0 +1,41 @@
+package com.project.method;
+
+public class demo {
+	void fly() {System.out.println("birds are fly");
+		
+	}
+	void eat() {
+		System.out.println("humans are eat foods");
+		
+	}
+	void play() {
+		System.out.println("they are playing cricket");
+		
+	}
+	
+	void write() {
+		System.out.println("they write poet");
+		
+	}
+	public String name() {
+		return"pavan";
+	}
+	int add() {
+		int a = 10;
+		int b = 20;
+		return a+b;
+	}
+	public float sum() {
+		float a=10;
+		float b=40;
+		return a+b; 
+	}
+	public long mobno() {
+		return 8778210873l;
+	}
+	double salary() {
+		return 25800;
+		
+	}
+
+}
---
+++ method/src/com/project/method/tester.java
@@ -0,0 +1,15 @@
+package com.project.method;
+
+public class tester {
+	public static void main(String[]args) {
+	demo demo =new demo();
+	demo.eat();
+	demo.fly();
+	demo.play();
+	demo.write();
+	System.out.println(demo.name());
+	System.out.println(demo.add());
+	System.out.println(demo.sum());
+	System.out.println(demo.mobno());
+System.out.println(demo.salary());
+}}
